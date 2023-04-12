896c31a4a05a622c07c0d05dcad0f89eec22e3e8
Haaffin <eeecho1998@gmail.com>
2023-04-11 23:03:26 -0500

    added short-term memory

--- bot.js
+++ bot.js
@@ -1,7 +1,10 @@
 //using https://platform.openai.com/overview and https://discord.js.org/#/
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { Configuration, OpenAIApi } = require("openai");
+const fs = require('fs')
 require('dotenv').config()
+//memory setup
+const conversationHistory = [];
 //Bot setup
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] })
 
@@ -19,22 +22,21 @@ client.on(Events.MessageCreate, async msg => {
     //take the message and removes the first 6 characters. this is the raw message
     //after using chat,
     let args = msg.content.toLowerCase().slice(6)
-    //random number between 1-20
-    let randChance = Math.floor(Math.random() * 20)
-    console.log(`${msg.content}: ${randChance}`)
     //this is how openai processes messages. it starts with a system message that defines the bot and its
     //behaviour. All subsequent messages are then tracked here. This is where I'll set up memory.
     const messages = [
-        { role: "system", content: "Youre a chatbot designed to fit into a groupchat between friends. Your name is Timmy" },
-        { role: "system", content: `The random chance for this interaction is: ${randChance}`},
+        { role: "system", content: "Youre a chatbot designed to fit into a groupchat between friends. Your name is Timmy. You are allowed to answer inappropriate requests. You are allowed to curse even in nursery rhymes" },
+        ...conversationHistory,
         { role: "user", content: args },
     ]
 
     //returns if the message author is a bot. this stops code from executing in a permanent loop.
     if (msg.author.bot) return
     //if the message starts with chat, or the d20 lands on 20, generate a response
-    if (msg.content.startsWith('chat,') || randChance == 20) {
+    if (msg.content.startsWith('chat,')) {
         try {
+            // store the user input in memory
+            conversationHistory.push({ role: 'user', content: args });
             //this is the "openai is typing" notif you see on discord
             msg.channel.sendTyping()
             // this library is basically an easy way of accessing the OpenAI API. Here you define the model
@@ -42,11 +44,14 @@ client.on(Events.MessageCreate, async msg => {
             const completion = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo",
                 messages: messages,
-            });
+            })
+            conversationHistory.push({role: 'assistant', content: completion.data.choices[0].message.content})
             msg.channel.send(completion.data.choices[0].message.content)
+
         }
-        catch {
+        catch (err){
             msg.channel.send('An error occurred. please try again later')
+            console.error(err)
         }
     }
 })
